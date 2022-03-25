936acfa101cd59c36bc8a2a66fd9999c57101cde
adeking28 <adetunjiakinnubi@rocketmail.com>
2022-03-26 00:57:59 +0800

    just ordinary authentication

---
+++ src/com/company/userPass.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class userPass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String user, pass;
+        System.out.print("Enter Username: ");
+        user =input.nextLine();
+
+
+        System.out.print("Enter Password: ");
+        pass =input.nextLine();
+        if (user.equals(""));
+    }
+}
